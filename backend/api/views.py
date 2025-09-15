692f22b64365bcccc9bd079b0f33a4358256f897
Aklilu Mandefro <hello@aklilumandefro.com>
2025-09-16 00:27:04 +0300

    Create views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Trip
+from .serializers import TripSerializer
 
-# Create your views here.
+# create a view to handle the trip data
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print(serializer.errors)  # Log validation errors
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
